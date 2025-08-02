3244bd7c877ba7c832a118f17769d3c1412ddc00
David Adewole <adeoluwadavid@gmail.com>
2025-08-02 14:58:08 +0100

    trigger job 3

--- ec2_deploy.sh
+++ ec2_deploy.sh
@@ -4,10 +4,13 @@ EC2_USER=ubuntu
 EC2_HOST=44.203.172.65
 KEY_PATH=/root/.ssh/my-key.pem
 
+# Upload app and requirements.txt
 scp -i $KEY_PATH -o StrictHostKeyChecking=no -r ./app $EC2_USER@$EC2_HOST:/home/ubuntu/
+scp -i $KEY_PATH -o StrictHostKeyChecking=no ./requirements.txt $EC2_USER@$EC2_HOST:/home/ubuntu/
 
+# SSH and run commands
 ssh -i $KEY_PATH -o StrictHostKeyChecking=no $EC2_USER@$EC2_HOST << 'ENDSSH'
   cd /home/ubuntu/app
-  pip3 install -r /home/ubuntu/requirements.txt
-  nohup uvicorn main:app --host 0.0.0.0 --port 80 --reload &
+  pip3 install --user -r /home/ubuntu/requirements.txt
+  nohup python3 -m uvicorn main:app --host 0.0.0.0 --port 80 --reload > fastapi.log 2>&1 &
 ENDSSH
